b2797aa138fd65e61ab9f594a5569b9770570856
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2019-08-31 18:59:39 -0600

    Lecture 22: Comment Challenge - Part III

--- src/index.js
+++ src/index.js
@@ -48,26 +48,32 @@ const posts = [
 ];
 
 // dummy comments
+// added an author id to identify who wrote the comment
+// also added a post id to identify what post owns this comment
 const comments = [
     {
         id: '100',
         text: 'This is pure non-sense.',
-        author: '2'
+        author: '2',
+        post: '10'
     },
     {
         id: '101',
         text: 'How did you formulate this opinion?',
-        author: '3'
+        author: '3',
+        post: '12'
     },
     {
         id: '102',
         text: 'Lol that was not funny.',
-        author: '3'
+        author: '3',
+        post: '11'
     },
     {
         id: '103',
         text: 'Kono Dio Da!',
-        author: '1'
+        author: '1',
+        post: '11'
     }
 ];
 
@@ -100,6 +106,9 @@ const comments = [
 
 // added to the 'Comment' type an 'author' field that holds the User that wrote such Comment
 // added to the 'User' type a 'comments' field that holds an array of Comment objects that the User wrote
+
+// added to the 'Comment' type a 'post' field that holds the Post object that owns that comment
+// added to the 'Post' type a 'comments' field that holds the Comments related to that Post
 const typeDefs = `
 
     type Query {
@@ -125,12 +134,14 @@ const typeDefs = `
         body: String!
         published: Boolean!
         author: User!
+        comments: [Comment!]!
     }
 
     type Comment {
         id: ID!
         text: String!
         author: User!
+        post: Post!
     }
 
 `;
@@ -212,9 +223,17 @@ const resolvers = {
     // resolver when accessing a User given a Post: get User by matching its primary key (id)
     // with the Post's author foreign key (owner, parent)
     Post: {
+
         author(parent, args, ctx, info) {
             return users.find(user => user.id === parent.author); 
+        },
+
+        // resolver when accessing Comments given a particular Post, get Comments where its 'post' foreign key
+        // match the parent's (post) primary key id
+        comments(parent, args, ctx, info) {
+            return comments.filter(comment => comment.post === parent.id);
         }
+
     },
 
     // resolver when accessing Posts given a User, get all Posts that match their foreign key with the
@@ -235,9 +254,17 @@ const resolvers = {
 
     // resolver when accessing User given a Comment: find User that matches its primary key (id) with the comments id (parent)
     Comment: {
+
         author(parent, args, ctx, info) {
             return users.find(user => user.id === parent.author);
+        },
+
+        // resolver when accessing a Post given a Comment, search for the post where its primary key (id) matches the parent 'post'
+        // foreign key (Comment)
+        post(parent, args, ctx, info) {
+            return posts.find(post => post.id === parent.post);
         }
+
     }
 
 };
